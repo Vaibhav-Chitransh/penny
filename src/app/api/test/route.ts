97cbb48b58c8ed7249fac3898d5af2163345e4b2
Vaibhav-Chitransh <vaibhavchitranshbdn@gmail.com>
2026-07-07 18:40:47 +0530

    Revert "clerk js setup for auth"
    This reverts commit 037c6f0f16b3cb13069fc6a99aed1245fd462f8b.

--- src/app/api/test/route.ts
+++
@@ -1,7 +0,0 @@
-import connectDB from "@/lib/db";
-import { NextResponse } from "next/server";
-
-export const GET = async () => {
-    await connectDB();
-    return NextResponse.json({ message: 'DB connect ho gaya' });
-}
