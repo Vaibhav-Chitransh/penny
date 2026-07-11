93c338db0d9d3bfbfd6109903f6d80bf3c14781a
Vaibhav-Chitransh <vaibhavchitranshbdn@gmail.com>
2026-07-11 21:17:27 +0530

    get transaction api with pagination, search and filters

--- src/app/api/transactions/route.ts
+++ src/app/api/transactions/route.ts
@@ -5,6 +5,7 @@ import Transaction from "@/models/transaction.model";
 import User from "@/models/user.model";
 import { CreateTransactionSchema } from "@/validators/transaction.validator";
 import { auth } from "@clerk/nextjs/server";
+import mongoose from "mongoose";
 import { NextRequest } from "next/server";
 
 export async function POST(req : NextRequest) {
@@ -51,5 +52,127 @@ export async function POST(req : NextRequest) {
     } catch (error) {
         console.error("Create Transaction error: ", error);
         return errorResponse("Internal server error", error, 500);
+    }
+}
+
+export async function GET(req : NextRequest) {
+    try {
+        // authenticate user
+        const {userId} = await auth();
+        if(!userId) return errorResponse("Unauthorized", null, 401);
+
+        // connect database
+        connectDB();
+
+        // find current mongo user
+        const mongoUser = await User.findOne({clerkId: userId}).select("_id");
+        if(!mongoUser) return errorResponse("User not found", null, 404);
+
+        // read query parameters
+        const {searchParams} = new URL(req.url);
+
+        // pagination
+        const page = Number(searchParams.get("page")) || 1;
+        const limit = Number(searchParams.get("limit")) || 10;
+
+        // filters
+        const type = searchParams.get("type");
+        const category = searchParams.get("category");
+
+        // search
+        const search = searchParams.get("search");
+
+        // date filters
+        const from = searchParams.get("from");
+        const to = searchParams.get("to");
+
+        // sorting
+        const sortBy = searchParams.get("sortBy") || "date";
+        const sortOrder = searchParams.get("sortOrder") == "asc" ? 1 : -1;
+
+        // pagination calculation
+        const skip = (page - 1) * limit;
+
+        // create aggregation pipeline
+        const pipeline: mongoose.PipelineStage[] = [];
+
+        // filter current user
+        pipeline.push({$match: {user: mongoUser._id}});
+
+        // join category table
+        pipeline.push({$lookup: {
+            from: "categories",
+            localField: "category",
+            foreignField: "_id",
+            as: "category",
+        }});
+
+        // remove category array (convert array -> object)
+        pipeline.push({$unwind: "$category"});
+
+        // type filter
+        if(type) pipeline.push({$match: {type}});
+
+        // category filter
+        if(category) {
+            if(!mongoose.Types.ObjectId.isValid(category)) {
+                return errorResponse("Invalid category id", null, 400);
+            }
+
+            pipeline.push({$match: {"category._id": new mongoose.Types.ObjectId(category)}});
+        }
+
+        // date filter
+        if(from || to) {
+            const dateFilter: {
+                $gte?: Date;
+                $lte?: Date;
+            } = {};
+
+            if(from) dateFilter.$gte = new Date(from);
+            if(to) dateFilter.$lte = new Date(to);
+
+            pipeline.push({$match: {date: dateFilter}});
+        }
+
+        // search
+        if(search) {
+            pipeline.push({$match: {
+                $or: [{
+                    note: {
+                        $regex: search,
+                        $options: "i",
+                    },
+                }, {
+                    "category.name": {
+                        $regex: search,
+                        $options: "i",
+                    }
+                }]
+            }})
+        }
+
+        // sorting
+        pipeline.push({$sort: {
+            [sortBy]: sortOrder,
+        }});
+
+        // facet
+        pipeline.push({$facet: {
+            transactions: [{$skip: skip}, {$limit: limit}],
+            totalCount: [{$count: "count"}]
+        }});
+
+        // execute the aggregation pipeline
+        const result = await Transaction.aggregate(pipeline);
+        const transactions = result[0]?.transactions ?? [];
+
+        const total = result[0]?.totalCount?.[0]?.count ?? 0;
+
+        // return success response
+        return successResponse("Transaction fetched successfully", {transactions, pagination: {page, limit, total, totalPages: Math.ceil(total / limit)}});
+    } catch (error) {
+        console.error("Get Transaction error: ", error);
+        return errorResponse("Internal Server error", error, 500);
     }
 }
